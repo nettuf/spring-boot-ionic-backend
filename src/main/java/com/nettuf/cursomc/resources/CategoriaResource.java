a35b1e1a0f0e0af4b6d183c6004c116ba22a9383
Francisco de Paula Fernandes Netto <hugkhj@gmail.com>
2021-08-04 18:44:05 -0300

    deleting one category with delete method

--- src/main/java/com/nettuf/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/nettuf/cursomc/resources/CategoriaResource.java
@@ -4,6 +4,7 @@ import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -44,4 +45,11 @@ public class CategoriaResource {
 		return ResponseEntity.noContent().build();
 	}
 	
-}
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Categoria> delete(@PathVariable Integer id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+		
+	}
+
--- src/main/java/com/nettuf/cursomc/services/CategoriaService.java
+++ src/main/java/com/nettuf/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.nettuf.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.nettuf.cursomc.domain.Categoria;
 import com.nettuf.cursomc.repositories.CategoriaRepository;
+import com.nettuf.cursomc.services.exceptions.DataIntegrityException;
 import com.nettuf.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -29,6 +31,16 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+		repo.deleteById(id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir esta categoria");
+		}
 	}
 	
 }
