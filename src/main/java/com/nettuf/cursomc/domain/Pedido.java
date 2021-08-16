8a069f3ed15f01c09123e7241bf319683f8d98a0
Francisco de Paula Fernandes Netto <hugkhj@gmail.com>
2021-08-16 15:35:21 -0300

    subtotal and total pedido and itempedido

--- src/main/java/com/nettuf/cursomc/domain/Pedido.java
+++ src/main/java/com/nettuf/cursomc/domain/Pedido.java
@@ -54,7 +54,15 @@ public class Pedido implements Serializable{
 		this.cliente = cliente;
 		this.enderecoDeEntrega = enderecoDeEntrega;
 	}
-
+	
+	public double getValorTotal() {
+		double soma = 0.0;
+		for (ItemPedido x : itens) {
+			soma += x.getSubTotal();
+	}
+		return soma;
+	}
+	
 	public Integer getId() {
 		return id;
 	}
