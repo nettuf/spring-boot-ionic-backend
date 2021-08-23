32613f0b839c0a85863a054fa3e6aa2d0c3a8723
Francisco de Paula Fernandes Netto <hugkhj@gmail.com>
2021-08-23 19:13:23 -0300

    inserting pedidos

--- src/main/java/com/nettuf/cursomc/resources/PedidoResource.java
+++ src/main/java/com/nettuf/cursomc/resources/PedidoResource.java
@@ -1,13 +1,22 @@
 package com.nettuf.cursomc.resources;
 
+import java.net.URI;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.nettuf.cursomc.domain.Categoria;
 import com.nettuf.cursomc.domain.Pedido;
+import com.nettuf.cursomc.dto.CategoriaDTO;
 import com.nettuf.cursomc.services.PedidoService;
 
 @RestController
@@ -23,4 +32,11 @@ public class PedidoResource {
 		return ResponseEntity.ok().body(obj);
 	}
 	
+	@PostMapping
+	public ResponseEntity<Void> insert(@Valid @RequestBody Pedido obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
 }
--- src/main/java/com/nettuf/cursomc/services/PedidoService.java
+++ src/main/java/com/nettuf/cursomc/services/PedidoService.java
@@ -1,11 +1,17 @@
 package com.nettuf.cursomc.services;
 
+import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.nettuf.cursomc.domain.ItemPedido;
+import com.nettuf.cursomc.domain.PagamentoComBoleto;
 import com.nettuf.cursomc.domain.Pedido;
+import com.nettuf.cursomc.domain.enums.EstadoPagamento;
+import com.nettuf.cursomc.repositories.ItemPedidoRepository;
+import com.nettuf.cursomc.repositories.PagamentoRepository;
 import com.nettuf.cursomc.repositories.PedidoRepository;
 import com.nettuf.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -14,10 +20,42 @@ public class PedidoService {
 	
 	@Autowired
 	private PedidoRepository repo;
-
+	
+	@Autowired
+	private BoletoService boletoService;
+	
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
+	
+	@Autowired
+	private ProdutoService produtoService;
+	
 	public Pedido find(Integer id) {
 		Optional<Pedido> obj = repo.findById(id);
-		return obj.orElseThrow(()-> new ObjectNotFoundException(
-				"Object not found! id:"+id+", tipo:"+Pedido.class.getName()));
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+	
+	public Pedido insert(Pedido obj) {
+		obj.setId(null);
+		obj.setInstante(new Date());
+		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
+		obj.getPagamento().setPedido(obj);
+		if (obj.getPagamento() instanceof PagamentoComBoleto) {
+			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
+			boletoService.preencherPagamentoComBoleto(pagto, obj.getInstante());
+		}
+		obj = repo.save(obj);
+		pagamentoRepository.save(obj.getPagamento());
+		for (ItemPedido ip : obj.getItens()) {
+			ip.setDesconto(0.0);
+			ip.setPreco(produtoService.find(ip.getProduto().getId()).getPreco());
+			ip.setPedido(obj);
+		}
+		itemPedidoRepository.saveAll(obj.getItens());
+		return obj;
 	}
 }
