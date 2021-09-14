20628f2a3d252cce9186fa127551c63e30456697
Francisco de Paula Fernandes Netto <hugkhj@gmail.com>
2021-09-14 12:26:44 -0300

    refresh token

--- src/main/java/com/nettuf/cursomc/resources/PedidoResource.java
+++ src/main/java/com/nettuf/cursomc/resources/PedidoResource.java
@@ -45,7 +45,7 @@ public class PedidoResource {
 			@RequestParam(value = "page", defaultValue = "0") Integer page,
 			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
 			@RequestParam(value = "orderBy", defaultValue = "instante") String orderBy,
-			@RequestParam(value = "direction", defaultValue = "DESC") String direction) {
+			@RequestParam(value = "dgirection", defaultValue = "DESC") String direction) {
 		Page<Pedido> list = service.findPage(page,linesPerPage, orderBy, direction);
 		
 		return ResponseEntity.ok().body(list);
