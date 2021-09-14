3c2c086c600b87a3ea61d07788b41e5c85669a93
Francisco de Paula Fernandes Netto <hugkhj@gmail.com>
2021-09-14 12:19:01 -0300

    client only recover their own orders

--- src/main/java/com/nettuf/cursomc/resources/PedidoResource.java
+++ src/main/java/com/nettuf/cursomc/resources/PedidoResource.java
@@ -5,18 +5,18 @@ import java.net.URI;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.nettuf.cursomc.domain.Categoria;
 import com.nettuf.cursomc.domain.Pedido;
-import com.nettuf.cursomc.dto.CategoriaDTO;
 import com.nettuf.cursomc.services.PedidoService;
 
 @RestController
@@ -38,5 +38,16 @@ public class PedidoResource {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
 				.buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
+	}
+	
+	@GetMapping
+	public ResponseEntity<Page<Pedido>> findPage(
+			@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+			@RequestParam(value = "orderBy", defaultValue = "instante") String orderBy,
+			@RequestParam(value = "direction", defaultValue = "DESC") String direction) {
+		Page<Pedido> list = service.findPage(page,linesPerPage, orderBy, direction);
+		
+		return ResponseEntity.ok().body(list);
 	}
 }
--- src/main/java/com/nettuf/cursomc/services/PedidoService.java
+++ src/main/java/com/nettuf/cursomc/services/PedidoService.java
@@ -4,8 +4,12 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.nettuf.cursomc.domain.Cliente;
 import com.nettuf.cursomc.domain.ItemPedido;
 import com.nettuf.cursomc.domain.PagamentoComBoleto;
 import com.nettuf.cursomc.domain.Pedido;
@@ -13,6 +17,8 @@ import com.nettuf.cursomc.domain.enums.EstadoPagamento;
 import com.nettuf.cursomc.repositories.ItemPedidoRepository;
 import com.nettuf.cursomc.repositories.PagamentoRepository;
 import com.nettuf.cursomc.repositories.PedidoRepository;
+import com.nettuf.cursomc.security.UserSS;
+import com.nettuf.cursomc.services.exceptions.AuthorizationException;
 import com.nettuf.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -66,5 +72,18 @@ public class PedidoService {
 		itemPedidoRepository.saveAll(obj.getItens());
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
+	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		UserSS user = UserService.authenticated();
+		if(user==null) {
+			throw new AuthorizationException("Acesso negado!");
+		}
+		
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage,
+			Direction.valueOf(direction), orderBy);
+		Cliente cliente = clienteService.find(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
+	
 	}
 }
