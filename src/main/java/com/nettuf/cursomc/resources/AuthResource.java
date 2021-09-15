dce3ef5c063e0298595a281d19b195aede6d25d3
Francisco de Paula Fernandes Netto <hugkhj@gmail.com>
2021-09-15 11:51:50 -0300

    forgot password

--- src/main/java/com/nettuf/cursomc/resources/AuthResource.java
+++ src/main/java/com/nettuf/cursomc/resources/AuthResource.java
@@ -1,15 +1,19 @@
 package com.nettuf.cursomc.resources;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.nettuf.cursomc.dto.EmailDTO;
 import com.nettuf.cursomc.security.JWTUtil;
 import com.nettuf.cursomc.security.UserSS;
+import com.nettuf.cursomc.services.AuthService;
 import com.nettuf.cursomc.services.UserService;
 
 @RestController
@@ -19,12 +23,21 @@ public class AuthResource {
 	@Autowired
 	private JWTUtil jwtUtil;
 	
+	@Autowired
+	private AuthService service;
+	
 	
 	@RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
 	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
 		UserSS user = UserService.authenticated();
 		String token = jwtUtil.generateToken(user.getUsername());
 		response.addHeader("Authorization", "Bearer " + token);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value = "/forgot", method = RequestMethod.POST)
+	public ResponseEntity<Void> forgot(@Valid @RequestBody EmailDTO objDTO) {
+		service.sendNewPassword(objDTO.getEmail());
 		return ResponseEntity.noContent().build();
 	}
 }
