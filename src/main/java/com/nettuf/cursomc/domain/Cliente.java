8d02c36c6749932b9a42b1162732c3bb3c554eaa
Francisco de Paula Fernandes Netto <hugkhj@gmail.com>
2021-09-23 14:49:31 -0300

    using pattern name in images.jpg

--- src/main/java/com/nettuf/cursomc/domain/Cliente.java
+++ src/main/java/com/nettuf/cursomc/domain/Cliente.java
@@ -59,7 +59,6 @@ public class Cliente implements Serializable{
 	@OneToMany(mappedBy = "cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
 	
-	private String imageUrl;
 	
 	@Override
 	public int hashCode() {
@@ -173,13 +172,7 @@ public class Cliente implements Serializable{
 		perfis.add(perfil.getCod());
 	}
 
-	public String getImageUrl() {
-		return imageUrl;
-	}
-
-	public void setImageUrl(String imageUrl) {
-		this.imageUrl = imageUrl;
-	}
+	
 	
 	
 }
